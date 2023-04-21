3c59fb114f28fd51f8439cb579af8f2339ae746f
Jim Enright <jenright@cloudera.com>
2023-04-21 09:22:57 +0100

    Revert Egress test
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- variables.tf
+++ variables.tf
@@ -204,7 +204,7 @@ variable "cdp_default_sg_egress_cidrs" {
 
   description = "List of egress CIDR blocks for CDP Default Security Group Egress rule"
 
-  default = ["35.80.24.128/27"]
+  default = ["0.0.0.0/0"]
 }
 
 variable "cdp_knox_sg_egress_cidrs" {
@@ -212,7 +212,7 @@ variable "cdp_knox_sg_egress_cidrs" {
 
   description = "List of egress CIDR blocks for CDP Knox Security Group Egress rule"
 
-  default = ["35.80.24.128/27"]
+  default = ["0.0.0.0/0"]
 }
 
 # ------- Storage Resources -------
